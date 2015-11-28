c234ff57a875ec9477f946b5e81b685415b3fdd3
George H. Peden <georgep@slalom.com>
2015-11-28 14:26:22 -0800

    add full list of quotes. plumb /quotes

--- hello.go
+++ hello.go
@@ -6,12 +6,30 @@ import (
 	"github.com/gpeden/hello-go/Godeps/_workspace/src/github.com/julienschmidt/httprouter"
 	"net/http"
 	"os"
+  "strconv"
 )
 
+
+var quotes = []Quote{
+    {Quote: "Just one thing, Dude. Do ya have to use so many cuss words?",Id: "1"},
+    {Quote: "Hello. Mein dizbatcher says zere iss somezing wrong mit deine kable.",Id: "2"},
+    {Quote: "Vee vant zat money, Lebowski.", Id:"3"},
+    {Quote: "Uh, yeah. Probably a vagrant, slept in the car. Or perhaps just used it as a toilet and moved on.", Id:"4" },
+    {Quote: "Your name is Lebowski, Lebowski. Your wife is Bunny.", Id:"5"},
+    {Quote: "You are joking. But perhaps you are right.", Id:"6"},
+    {Quote: "I'm staying. I'm finishing my coffee. Enjoying my coffee.", Id:"7"},
+    {Quote: "Tomorrow vee come back and cut off your Johnson.", Id:"8"},
+    {Quote: "Has the whole world gone CRAZY?! Am I the only one who gives a shit about the rules?! MARK IT ZERO!", Id:"8"},
+
+}
+
 func main() {
+
+
 	router := httprouter.New()
 	router.GET("/", HelloHandler)
 	router.GET("/quote/:id", QuoteHandler)
+  router.GET("/quotes", QuotesHandler)
 	fmt.Println("listening...")
 
 	http.Handle("/", router)
@@ -25,14 +43,24 @@ func HelloHandler(res http.ResponseWriter, req *http.Request, p httprouter.Param
 	fmt.Fprintln(res, "go: HelloHandler: hello, world!!!")
 }
 
+
+
 func QuoteHandler(res http.ResponseWriter, req *http.Request, p httprouter.Params) {
 	// Stub an example quote
-	q := Quote{
-		Quote: "hey this is a private residence man!",
-		Id:    p.ByName("id"),
-	}
+  id, _ := strconv.Atoi(p.ByName("id"))
+
+	q := quotes[id - 1]
 
 	qj, _ := json.Marshal(q)
+
+	// Write content-type, statuscode, payload
+	res.Header().Set("Content-Type", "application/json")
+	res.WriteHeader(200)
+	fmt.Fprintf(res, "%s", qj)
+}
+
+func QuotesHandler(res http.ResponseWriter, req *http.Request, p httprouter.Params) {
+	qj, _ := json.Marshal(quotes)
 
 	// Write content-type, statuscode, payload
 	res.Header().Set("Content-Type", "application/json")
