ac6dd774c502992795f6a661470ea098d5719796
George H. Peden <georgep@slalom.com>
2015-11-28 13:06:47 -0800

    fix routers

--- hello.go
+++ hello.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "os"
-    "github.com/gorilla/mux"
+	"fmt"
+	"github.com/gpeden/hello-go/Godeps/_workspace/src/github.com/gorilla/mux"
+	"net/http"
+	"os"
 )
 
 func main() {
-    router := mux.NewRouter().StrictSlash(true)
-    router.HandleFunc("/", HelloHandler)
-    router.HandleFunc("/quote", QuoteHandler)
-    fmt.Println("listening...")
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", HelloHandler)
+	router.HandleFunc("/quote", QuoteHandler)
+	fmt.Println("listening...")
 
-    http.Handle("/", router)
-    err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-    if err != nil {
-      panic(err)
-    }
+	http.Handle("/", router)
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func HelloHandler(res http.ResponseWriter, req *http.Request) {
-    fmt.Fprintln(res, "go: HelloHandler: hello, world!!!")
+	fmt.Fprintln(res, "go: HelloHandler: hello, world!!!")
 }
 
 func QuoteHandler(res http.ResponseWriter, req *http.Request) {
-    fmt.Fprintln(res, "hey this is a private residence man!")
+	fmt.Fprintln(res, "hey this is a private residence man!")
 }
